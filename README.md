e5acc0e6827ecd258e5ce53c218c4e939c9d075e
Igor Rubanov <fomagreen@yandex.ru>
2023-05-09 16:47:35 +0300

    Check CI

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Selenium
+# Selenium[![Build status](https://ci.appveyor.com/api/projects/status/p2j9u1v1nm9psm42?svg=true)](https://ci.appveyor.com/project/rubbannov/selenium)
 Testing web interface with Selenium
--- src/test/java/TestForm.java
+++ src/test/java/TestForm.java
@@ -1,3 +1,50 @@
-public class TestForm {
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class TestForm {
+    WebDriver driver;
+
+    @BeforeAll
+    static void setupClass() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    void setupTest() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+        driver = new ChromeDriver();
+    }
+
+    @AfterEach
+    void teardown() {
+        driver.quit();
+    }
+
+    @Test
+    void testForm() {
+        driver.get("http://localhost:9999/");
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Иван Васильев");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input ")).sendKeys("+79990999999");
+        driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
+        driver.findElement(By.cssSelector("[role='button']")).click();
+
+        String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        String actual = driver.findElement(By.cssSelector("[data-test-id='order-success']")).getText().trim();
+
+        assertEquals(expected, actual);
+    }
 }
